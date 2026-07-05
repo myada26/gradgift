ac550cc088842d9673d4c6d3f509fb1c820499b5
myada26 <myadacapstone@gmail.com>
2026-07-06 01:28:55 +0800

    Update Andrea and KC messages

--- assets/andrea/content.js
+++ assets/andrea/content.js
@@ -1,5 +1,5 @@
 window.FlowerRevealContent = {
   "header1": "Congrats Momsh!",
   "header2": "Sexbomb Andrea!",
-  "paragraph": "We don't talk as much as I'd like, and there's a joke you made that stung more than you probably meant it to. But I still see the part of you that actually appreciates me — and that's the part I'm choosing to believe."
+  "paragraph": "We don't talk as much as I'd like, and some things you've said have landed harder than you probably meant. But I still see the part of you that appreciates me — and that's the part I choose to hold onto."
 };
